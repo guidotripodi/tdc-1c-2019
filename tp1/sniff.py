ada533e283a2c6298de92a9543ef34109c3d2959
Gonzalo Guillamon <gonzaguillamon@gmail.com>
2019-04-21 16:56:02 -0300

    subo sniffer

---
+++ tp1/sniff.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+from scapy.all import *
+import os
+import socket
+from datetime import datetime
+
+# Seteo modo promiscuo
+conf.sniff_promisc = True
+
+if len(sys.argv) == 3:
+
+    ftm="YYYYBBDDHHSS"
+    d1 = datetime.now()
+    paquetes = sniff( iface=sys.argv[1],store=1, timeout = 15000)
+    d2 = datetime.now()
+
+    diff = d2 - d1
+    diff_minutos = diff.seconds/60
+    nombreArchivo = 'sniff_{}_{}_{}.pcap'.format(sys.argv[2],datetime.now().strftime("%Y%m%d-%H%M%S"),diff_minutos)
+    wrpcap(nombreArchivo,paquetes)
+
+
+
+else:
+    print("Introduzca el iface + nombre de la red")
