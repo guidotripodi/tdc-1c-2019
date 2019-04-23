7fe58f57acc47c4a997187fa6d10ee73c2c90396
guidotripodi <guido.tripodi@hotmail.com>
2019-04-23 00:35:13 -0300

    subiendo

---
+++ tp1/scapy_graph.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python2.7
+import argparse
+from tp1 import saveFigure
+from plotter import Plotter
+from sources import * 
+from scapy.all import rdpcap
+
+if __name__ == "__main__":
+    #Parse command line arguments
+	parser = argparse.ArgumentParser(description='Script for plotting network packet related data.')
+	parser.add_argument("file", help="Pcap formatted capture")
+	args = parser.parse_args()
+
+	pcap = rdpcap(args.file)
+	S1 = Source1(pcap)
+	plotter = Plotter(S1)
+    
+	saveFigure(plotter.probabilityPlot(),args.file, 1)
+	saveFigure(plotter.informationPlot(),args.file, 1)
+	saveFigure(plotter.distributionPlot(),args.file, 1)
+
+	S2 = Source2(pcap)
+	plotter = Plotter(S2)
+	
+	saveFigure(plotter.probabilityPlot(),args.file, 2)
+	saveFigure(plotter.informationPlot(),args.file, 2)
--- tp1/tp1.py
+++ tp1/tp1.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python
 
-import sys, os
+
 from math import log as LOG
+import argparse 
+#from helpers import * 
+from scapy.all import rdpcap                                              
 from scapy.all import *
-import argparse
+from sets import Set
+from collections import Counter
 import math
 import csv
 import os
-import collections
+from helpers import expand
+from collections import Counter
+from math import log
+from helpers import Ei
+import plotly.plotly as py
 tramas = 0
 
 
@@ -119,6 +127,46 @@ class Source2():
 	def name(self):
 		return "Fuente 2"
 
+
+def expand(x):
+    yield x.name
+    while x.payload:
+        x = x.payload
+        yield x.name
+'''
+Encapsulation of division for clarification
+'''
+def P(x, total):
+	return x/float(total)
+
+'''
+Given a probability of a symbol returns the information of the symbol 
+'''
+def I(p):
+	return -math.log(p, 2)
+
+'''
+Symbol entropy
+'''
+def Ei(x, total):
+	pi = P(x, total)
+	return pi*I(pi)
+
+'''
+Plots figure into the file named <plot_name>_<input_file_name> 
+'''
+def saveFigure(figure, input_file_name, sourceNumber):
+	directory = 'graficos/'
+
+	source = "Source" + str(sourceNumber)
+
+	if not os.path.exists(directory):
+		os.makedirs(os.path.dirname(directory))	
+    
+	folders = input_file_name.split('/')
+	input_file_name = folders[len(folders) - 1]
+	name = directory + figure.name() + "_" + input_file_name.replace('.pcap', "_" + source + '.png') 
+	py.image.save_as(figure.figure, filename=name)
 
 
 #no filtro por arp
